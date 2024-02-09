d3fc0034f6bf6c2a9f66130fa9a8e54146d4f4e9
방가온 <128475660+gaoooon@users.noreply.github.com>
2024-02-09 22:34:58 +0900

    [level 1] Title: 내적, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -0,0 +1,6 @@
+function solution(a, b) {
+  const multiply = a.map((item, index) => item * b[index]);
+  const answer = multiply.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+    
+  return answer; 
+}
