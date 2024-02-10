7ad26f569d33d47105066a791808f3ed249e723a
방가온 <128475660+gaoooon@users.noreply.github.com>
2024-02-10 23:45:27 +0900

    [level 0] Title: 암호 해독, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,6 @@
+function solution(cipher, code) {
+  const answer = [];
+  [...cipher].forEach((item, index) => { (index + 1) % code === 0 && answer.push(item)});
+    
+  return answer.join("");    
+}
